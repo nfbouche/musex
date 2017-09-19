60e31ed50578042239d1065a8cef2a12398d4477
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-09-19 17:38:51 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mouse',
+    version='0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['numpy', 'matplotlib', 'astropy', 'mpdaf', 'aplpy',
+                      'lineid_plot', 'joblib', 'dataset', 'scipy'],
+    # scripts=[],
+    # entry_points={
+    #     'console_scripts': [
+    #         'fix-icrs = muse_analysis.scripts.fix_icrs:main'
+    #     ],
+    # },
+)
