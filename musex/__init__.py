28f958fb377291abdf4feebc30d26844ae887c16
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-09-21 17:29:20 +0200

    Split config, add banner

--- musex/__init__.py
+++ musex/__init__.py
@@ -3,5 +3,11 @@ from mpdaf.log import setup_logging
 
 from .hstprior import *  # noqa
 from .settings import *  # noqa
+from .version import __version__, __description__
 
 setup_logging(__name__, level=logging.DEBUG)
+
+if conf['show_banner']:  # noqa
+    print("""
+    MUSEX, {} - v{}
+    """.format(__description__, __version__))
