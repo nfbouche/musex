b8cd488c52d25f574c892aed59d325272d4f0fc1
Simon Conseil <simon.conseil@univ-lyon1.fr>
2017-11-24 16:06:42 +0100

    Update version number at import

--- musex/version.py
+++ musex/version.py
@@ -3,9 +3,26 @@
 __version__ = '0.1.dev'
 __description__ = 'The MUse Source EXtractor :)'
 
+
+def _update_git_version():
+    import subprocess
+    command_number = 'git rev-list --count HEAD'
+    try:
+        commit_number = subprocess.check_output(command_number, shell=True)\
+            .decode('ascii').strip()
+    except Exception:
+        pass
+    else:
+        return commit_number
+
+
 try:
-    from ._githash import __githash__, __dev_value__
     if '.dev' in __version__:
-        __version__ += __dev_value__
+        commit_number = _update_git_version()
+        if commit_number:
+            __version__ += commit_number
+        else:
+            from ._githash import __dev_value__
+            __version__ += __dev_value__
 except Exception:
     pass
