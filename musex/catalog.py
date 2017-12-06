Fix center computation for mask_obj

musex/catalog.py
@@ -372,8 +372,8 @@ class Catalog(BaseCatalog):
             else:
                 debug('source %05d (%.5f, %.5f), extract mask', id_, ra, dec)
                 center = (dec, ra)
-                centerpix = wcsref.sky2pix(center)[0]
                 mask = get_mask(id_, center, mask_size)
+                centerpix = mask.wcs.sky2pix(center)[0]
                 mask.regrid(newdim, center, centerpix, inc, order=0,
                             unit_inc=usize, inplace=True, antialias=False)
                 mask._data = np.around(mask._data).astype(np.uint8)